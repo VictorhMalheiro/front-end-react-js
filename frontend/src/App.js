0ab576dccdd88efd1bbd21b4dd8b1e31dc3cfd92
Vinicius Eduardo Malheiro <viniciusedu007sk8@gmail.com.br>
2020-07-25 17:41:34 -0300

    Finalização - Conceitos do ReactJS ✅

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
 
 import Header from './components/Header';
 
 function App () {
-    const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-End de E-commerce']);
+    const [projects, setProjects] = useState([]);
+    const [inputTitle, setInputTitle] = useState('');
+    const [inputOwner, setInputOwner] = useState('');
     
-    function handleAddProject() {
-        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    
+    async function handleAddProject() {
+        // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+        const response = await api.post('projects', {
+            // title: inputTitle,
+            // owner: inputOwner,
+            title: `asd projeto ${Date.now()}`,
+            owner: 'Victor Malheiro',
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
     }
 
     return (
         <>
             <Header title="Home Page" />
-            
+            <hr/>
+            {/* <input type="text" onChange={() => setInputTitle(inputTitle)} value={inputTitle}/>
+            <input type="text" onChange={() => setInputOwner(inputOwner)} value={inputOwner}/> */}
             <ul>
-                {projects.map(project => <li key={project} >{project}</li>)}
+                {projects.map(project => <li key={project.id} >{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={() => handleAddProject()}>
