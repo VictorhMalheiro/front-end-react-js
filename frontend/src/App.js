0edd5d2d06aa76fe2ccb0c9595f572bf8a4b9bb8
Vinicius Eduardo Malheiro <viniciusedu007sk8@gmail.com.br>
2020-07-25 15:28:53 -0300

    Propriedades e Children

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,13 @@ import Header from './components/Header';
 function App () {
     return (
         <>
-            <Header />
+            <Header title="Home Page">
+                <ul>
+                    <li>Contato</li>
+                    <li>Sobre</li>
+                    <li>Projetos</li>
+                </ul>
+            </Header>
             <h1>Hello World</h1>
         </>
     );
